55aca0a0ba6be8faf304dfdeec7f581dd58fb8b4
sarra-94 <gusmi.sarra@gmail.com>
2022-01-04 14:22:04 +0100

    update jsx workshop js part

--- src/App.js
+++ src/App.js
@@ -8,41 +8,10 @@ function App() {
   const myStyle = {
     color: "blue",
   };
+  const products = ["product1", "product2", "product3", "product4"];
 
   return (
-    /**
-     * JSX code
-     */
-    <div className="App">
-      <p onClick={() => alert("hello")} className="test-paragraph">
-        Hello paragraph
-      </p>
-
-      {/* use expression to view data */}
-      <p style={{ color: "red", margin: "0 30%", backgroundColor: "black" }}>
-        {name}
-      </p>
-      <p style={myStyle}>{2 * 5}</p>
-
-      {/* Closing tags */}
-      <hr style={{ width: "50%" }} />
-
-      {/* Warning without the "alt" prop of "img" tag */}
-      <img
-        className="react-logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
-        alt="logo react"
-      />
-      <br />
-
-      {/* importing images */}
-      <img src="jsx.png" alt="" style={{ width: "30%" }} />
-      <br />
-      <br />
-      <br />
-      <img src={JSXImage} alt="" style={{ width: "30%" }} />
-    </div>
-
+    //  How Babel convert the code
     /**
      * JSX Code => JS code
      */
@@ -60,6 +29,59 @@ function App() {
     //   ),
     //   "Hello paragraph"
     // )
+    /**
+     * JSX code
+     */
+    <div className="App">
+      {/* use expression to view data */}
+      <p style={{ color: "red", margin: "0 30%", backgroundColor: "black" }}>
+        {name}
+      </p>
+      <p style={myStyle}>{2 * 5}</p>
+
+      {/* Closing tags */}
+      <hr style={{ width: "50%" }} />
+
+      {/* Warning without the "alt" prop of "img" tag */}
+      <img
+        className="react-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+        alt="logo react"
+      />
+      {/* impaired tags are closed */}
+      <br />
+
+      {/* importing images */}
+      <img src="jsx.png" alt="" style={{ width: "30%" }} />
+      <br />
+      <br />
+      <br />
+      <img src={JSXImage} alt="" style={{ width: "30%" }} />
+      {/* ----------------------------- */}
+      {/* event in JS */}
+      <p onClick={() => alert("hello")} className="test-paragraph">
+        Hello paragraph
+      </p>
+      {/* condition in JSX */}
+      <h2>{true ? "Hi" : "Bye"}</h2>
+      {/* operator in JSX */}
+      {true && (
+        <button>This button will appear when the condition is true</button>
+      )}
+      {/* Loop over an array in JSX */}
+      {products.map((el) => (
+        // function always return 1 element wrapped in a div
+        <div>
+          <h2>{el}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            delectus ut dolore sed praesentium sunt perferendis saepe.
+            Doloremque, et libero! Ad nobis, culpa iste placeat totam deserunt
+            cupiditate omnis modi.
+          </p>
+        </div>
+      ))}
+    </div>
   );
 }
 
